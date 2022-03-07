0f5fed4c5458598e47d46d14ec77c2b76b79bbbb
N. Murataj <59851929+niazi-m@users.noreply.github.com>
2022-03-06 21:22:01 -0500

    manual commit

--- src/mild/mild_1.js
+++ src/mild/mild_1.js
@@ -9,7 +9,7 @@
  * see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Template_literals
  */
 export function sumToString(a, b) {
-
+    return (a + ' + ' + b + ' = ' + (a+b));
 }
 
 
@@ -24,7 +24,11 @@ export function sumToString(a, b) {
  *
  */
 export function getIncreasingArray(startNumber, endNumber) {
-
+    let arr = new Array(endNumber - startNumber + 1);
+    for (let i = startNumber; i <= endNumber; i++ ) {
+        arr[i - startNumber] = i;
+    }
+    return arr;
 }
 
 /**
@@ -35,7 +39,10 @@ export function getIncreasingArray(startNumber, endNumber) {
  * and https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math
  */
 export function maxAndMin(numbers) {
-
+    let max = Math.max(...numbers);
+    let min = Math.min(...numbers);
+    const obj = { "max": max, "min": min};
+    return obj;
 }
 
 /**
@@ -49,5 +56,11 @@ export function maxAndMin(numbers) {
  *
  */
 export function countArray(array) {
-
+    let varb = {};
+    for(var i = 0; i < array.length; ++i) {
+        if(!varb[array[i]])
+            varb[array[i]] = 0;
+        ++varb[array[i]];
+    }
+    return varb;
 }
